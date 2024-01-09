524d091074c9bc1d3c6930b6c0ce3782f614839b
Daniel <daniel.rocha@multicomnet.com.br>
2024-01-09 20:28:35 -0300

    implementacao userredux e action-types

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useSelector, useDispatch} from "react-redux";
+import rootReducer from "../../redux/root-reducer";
+import UserActionTypes from "../../redux/action-types";
 
 // Components
 import Cart from "../cart/index";
@@ -6,18 +9,49 @@ import Cart from "../cart/index";
 // Styles
 import * as Styles from "./styles";
 
+
+
+
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  const handleCartClick = () => {
+
+const {currentUser} = useSelector(rootReducer => rootReducer.userReducer);
+const dispatch = useDispatch()
+
+console.log(currentUser);
+
+const handlerLoginClick = () =>{
+  dispatch({
+     type :  UserActionTypes.LOGIN
+     
+  });
+}
+
+
+
+const handlerLogoutClick = () =>{
+  dispatch({
+     type :  UserActionTypes.LOGOUT
+     
+  });
+
+}
+
+
+
+const handleCartClick = () => {
     setCartIsVisible(true);
   };
 
+
+
+
   return (
     <Styles.Container>
-      <Styles.Logo>Redux Shopping</Styles.Logo>
+      <Styles.Logo>Prova Daniel Rocha</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+       {currentUser ? (<div onClick={handlerLogoutClick}>Sair</div>) : ( <div onClick={handlerLoginClick}>Login</div>)  }
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,16 +1,25 @@
+
+import UserActionTypes from "../action-types";
+
 const initialState = {
    currentUser : null,
 
-}
+};
 
 const userReducer = (state = initialState, action) => {
+ 
+ 
+  switch (action.type) {
+   
+    case UserActionTypes.LOGIN : 
+       return {...state, currentUser: 10};
 
-  if(action.type === 'user/login'){
-       return {currentUser: 10};
+    case UserActionTypes.LOGOUT : 
+       return {...state, currentUser: null};
 
-
-  }
-
+    default : return state
+ }
+  
 };
 
 
